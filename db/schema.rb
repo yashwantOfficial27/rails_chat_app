f00cc5d7cdadab7262f9399811e5c12602232bd4
Yashwant Yadav <yadavyashwant.278@gmail.com>
2025-12-16 17:41:39 +0530

    Add postgreSQL for production

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_11_15_053550) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
-    t.integer "room_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
